b4d383a444a8ca905b0f4fdba6a8545c8d8dc57f
Chujo58 <chloelegue@gmail.com>
2026-01-08 00:30:56 +0100

    Changed some colors, though me thinks imma change them again

--- notion-lite/src/components/Card.jsx
+++ notion-lite/src/components/Card.jsx
@@ -1,10 +1,13 @@
-export default function Card({ title }) {
+export default function Card({ title, image }) {
   return (
-    <div className="bg-white rounded-lg shadow-sm border p-4 hover:shadow-md transition">
-      <h3 className="font-medium">{title}</h3>
-      <p className="text-sm text-neutral-500 mt-2">
-        Click to open
-      </p>
+    <div className="bg-snow text-abyss dark:bg-abyss dark:text-snow m-4 rounded-lg shadow hover:shadow-md hover:bg-frost/50 hover:dark:bg-abyss/50 cursor-pointer transition">
+      {/* Add image here for Card */}
+      {image ? (
+        <img src={image} alt={title} className="mb-2 w-full h-40 object-cover rounded-t-lg" />
+      ) : (
+        <div className="mb-2 w-full h-40 rounded-t-lg bg-mist" />
+      )}
+      <h3 className="font-medium font-sans p-4 pt-1">{title}</h3>
     </div>
   )
 }
--- notion-lite/tailwind.config.js
+++ notion-lite/tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx,html}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Rubik", "ui-sans-serif", "system-ui"],
+                body: ["Quicksand", "ui-sans-serif", "system-ui"],
+            },
+            colors: {
+                abyss: "#01162b", // deepest navy (backgrounds)
+                current: "#00385a", // primary UI blue
+                mist: "#6a90b4", // secondary / accents
+                frost: "#94a2bf", // muted text, borders
+                snow: "#d2dbeb", // light backgrounds
+            },
+        },
+    },
+    plugins: [],
+};
