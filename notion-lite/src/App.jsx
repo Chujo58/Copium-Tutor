998f82478477f91a3510074f607c0b461073e1b0
Jocelyne <jocelyneli0622@gmail.com>
2026-01-10 18:07:11 -0500

    new pages

--- notion-lite/src/App.jsx
+++ notion-lite/src/App.jsx
@@ -8,6 +8,9 @@ import {
 } from "react-router-dom";
 import { UserDashboard, LandingPage } from "./components/Page";
 import CoursePage from "./pages/CoursePage";
+import FlashcardsHomePage from "./pages/FlashcardsHomePage";
+import DeckPage from "./pages/DeckPage";
+
 
 
 export default function App() {
@@ -17,6 +20,8 @@ export default function App() {
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/dashboard" element={<UserDashboard />} />
                 <Route path="/project/:projectId" element={<CoursePage />} />
+                <Route path="/project/:projectId/flashcards" element={<FlashcardsHomePage />} />
+                <Route path="/project/:projectId/flashcards/:deckId" element={<DeckPage />} />
             </Routes>
         </Router>
     );
