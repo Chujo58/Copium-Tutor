13404f2f0e0a3c7c730285d5f11c07468316b1be
Chujo58 <chloelegue@gmail.com>
2026-01-10 17:36:58 +0100

    small fix to sidebar (adding a divider)

--- notion-lite/src/components/Sidebar.jsx
+++ notion-lite/src/components/Sidebar.jsx
@@ -12,6 +12,7 @@ import {
 
 export default function Sidebar({ projectsList }) {
     const [collapsed, setCollapsed] = useState(false);
+    const [openProfilePopout, setOpenProfilePopout] = useState(false);
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [pfp, setPfp] = useState("");
@@ -43,7 +44,9 @@ export default function Sidebar({ projectsList }) {
         >
             {/* Header */}
             <div className="flex items-center justify-between p-4">
-                {!collapsed && <h2 className="text-xl font-card main-header">Menu</h2>}
+                {!collapsed && (
+                    <h2 className="text-xl font-card main-header">Menu</h2>
+                )}
                 <button
                     onClick={() => setCollapsed(!collapsed)}
                     className="text-rose-plum hover:text-rose-copper hover:bg-dark/30 rounded-3xl focus:outline-none p-2"
@@ -54,7 +57,7 @@ export default function Sidebar({ projectsList }) {
             {/* First we should have the important links like dashboard and maybe something at the bottom for the user profile */}
             <nav
                 className={`flex flex-col h-full ${
-                    collapsed ? "items-center p-4 justify-between" : "p-2"
+                    collapsed ? "items-center justify-between" : ""
                 }`}
             >
                 <a
@@ -63,7 +66,7 @@ export default function Sidebar({ projectsList }) {
                     className={`flex items-center hover:bg-dark/30  ${
                         collapsed
                             ? "justify-center p-2 rounded-3xl hover:text-rose-copper"
-                            : "w-full  rounded m-2"
+                            : "w-full  rounded m-4"
                     }`}
                 >
                     <LayoutDashboard
@@ -72,9 +75,13 @@ export default function Sidebar({ projectsList }) {
                     {!collapsed && <span>Dashboard</span>}
                 </a>
                 {/* Add a list of all the projects */}
-                <div className="flex items-center justify-between p-2">
+                <div className="flex items-center justify-between p-2 mx-2">
                     {!collapsed && (
-                        <h2 className="main-header font-card">Projects</h2>
+                        <div className="flex flex-col w-full">
+                            <h2 className="main-header font-card">Projects</h2>
+                            <hr className="border-t-2 border-rose-copper" />
+
+                        </div>
                     )}
                 </div>
                 {Array.isArray(projectsList) && projectsList.length > 0
@@ -85,7 +92,7 @@ export default function Sidebar({ projectsList }) {
                               className={`flex items-center hover:bg-dark/30  ${
                                   collapsed
                                       ? "justify-center p-2 rounded-3xl hover:text-rose-copper"
-                                      : "w-full  rounded m-2"
+                                      : "w-60  rounded ml-2 mr-2 p-2"
                               }`}
                           >
                               {item.icon && (
@@ -98,9 +105,13 @@ export default function Sidebar({ projectsList }) {
                       ))
                     : ""}
                 {/* Now at the bottom, we should have the user profile */}
-                <div className="mt-auto flex p-2">
+                <div className="mt-auto flex p-4 bg-rose-dusty/40">
                     {/* Circle profile picture */}
-                    <img src={pfp} alt={`${firstName.charAt(0)} ${lastName.charAt(0)}`} className="rounded-full w-8 h-8 text-sm bg-dark text-accent text-center flex items-center justify-center main-header" />
+                    <img
+                        src={pfp}
+                        alt={`${firstName.charAt(0)} ${lastName.charAt(0)}`}
+                        className="rounded-full w-8 h-8 text-sm bg-dark text-accent text-center flex items-center justify-center main-header"
+                    />
                     {!collapsed && (
                         <div className="text-center flex items-center justify-center ml-4 font-card">
                             {`${firstName} ${lastName}`}
