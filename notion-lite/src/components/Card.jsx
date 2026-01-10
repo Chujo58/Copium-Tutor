70bb05c10d55384b775db715c0b3829f097b1c01
Chujo58 <chloelegue@gmail.com>
2026-01-10 21:47:05 +0100

    Fixing font color

--- notion-lite/src/components/Card.jsx
+++ notion-lite/src/components/Card.jsx
@@ -17,7 +17,7 @@ function getContrastTextColor(hex) {
     const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
 
     // Return black for bright colors, white for dark colors
-    return luminance > 0.5 ? "text-black" : "text-white";
+    return luminance > 0.5 ? "#000000" : "#FFFFFF";
 }
 
 function darkenHex(hex, amount = 0.1) {
