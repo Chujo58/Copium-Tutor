6781178b2449034198f0a6df5ac0898d151be856
Jocelyne <jocelyneli0622@gmail.com>
2026-01-10 18:16:54 -0500

    deck pages

---
+++ notion-lite/src/pages/DeckPage.jsx
@@ -0,0 +1,8 @@
+export default function DeckPage() {
+  return (
+    <div>
+      <h1>Deck Page</h1>
+      <p>(Deck details go here)</p>
+    </div>
+  );
+}
---
+++ notion-lite/src/pages/FlashcardsHomePage.jsx
@@ -0,0 +1,8 @@
+export default function FlashcardsHomePage() {
+  return (
+    <div>
+      <h1>Flashcards Home Page</h1>
+      <p>(Flashcards home page content goes here)</p>
+    </div>
+  );
+}
