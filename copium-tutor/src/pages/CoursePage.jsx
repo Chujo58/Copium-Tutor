2ced9c30b67f3b8aa2e234a85ded30db63ade3ad
Chloé Legué <chloelegue@gmail.com>
2026-01-22 01:48:14 +0100

    Fix to sidebar color

--- copium-tutor/src/pages/CoursePage.jsx
+++ copium-tutor/src/pages/CoursePage.jsx
@@ -323,6 +323,7 @@ export default function CoursePage() {
                 }}
                 activeProject={course}
                 projects={projects}
+                activeProject={course}
             />
             
 
