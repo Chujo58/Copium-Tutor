e3f5d15c1f454d1052aee17ac8567b2a380f73d9
Chloé Legué <chloelegue@gmail.com>
2026-01-22 02:01:45 +0100

    Fix: JSX problem

--- copium-tutor/src/pages/CoursePage.jsx
+++ copium-tutor/src/pages/CoursePage.jsx
@@ -323,7 +323,6 @@ export default function CoursePage() {
                 }}
                 activeProject={course}
                 projects={projects}
-                activeProject={course}
             />
             
 
